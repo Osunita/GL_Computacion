58c1749548172f08c5cdd009a532e547e4f7dad7
Osunita <raulosunaabadias71@gmail.com>
2025-02-13 17:08:37 +0100

    Suelo y pared

--- GL_ComptacionProyecto/Computación_GL/Computación_GL/Shader.h
+++ GL_ComptacionProyecto/Computación_GL/Computación_GL/Shader.h
@@ -7,6 +7,7 @@
 #include<sstream>
 #include<iostream>
 #include<cerrno>
+#include <glm/fwd.hpp>
 
 class Shader
 {
--- GL_ComptacionProyecto/Computación_GL/Computación_GL/main.cpp
+++ GL_ComptacionProyecto/Computación_GL/Computación_GL/main.cpp
@@ -108,6 +108,83 @@ GLuint indices[] =
 	13, 15, 14
 };
 
+GLfloat planeVertices[] =
+{ //     COORDINATES         /        COLORS          /    TexCoord   /        NORMALS      
+	-3.0f, -0.5f,  3.0f,     0.83f, 0.70f, 0.44f,  0.0f, 0.0f,      0.0f, 1.0f, 0.0f, // Vértice 0: Superior izquierda
+	 3.0f, -0.5f,  3.0f,     0.83f, 0.70f, 0.44f,	50.0f, 0.0f,      0.0f, 1.0f, 0.0f, // Vértice 1: Superior derecha
+	 3.0f, -0.5f, -3.0f,     0.83f, 0.70f, 0.44f,	50.0f, 50.0f,     0.0f, 1.0f, 0.0f, // Vértice 2: Inferior derecha
+	-3.0f, -0.5f, -3.0f,     0.83f, 0.70f, 0.44f,	 0.0f, 50.0f,     0.0f, 1.0f, 0.0f  // Vértice 3: Inferior izquierda
+};
+
+GLuint planeIndices[] =
+{
+	0, 1, 2, // Primer triángulo
+	0, 2, 3  // Segundo triángulo
+};
+
+// 24 vértices: 6 caras x 4 vértices por cara
+GLfloat wallVertices[] = {
+	// Front face (cara frontal, z = -10)
+	// Posición                 Color         TexCoords      Normal
+	-3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,    0.0f,  0.0f, 1.0f, // 0: inferior izquierda
+	 3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,    0.0f,  0.0f, 1.0f, // 1: inferior derecha
+	 3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,    0.0f,  0.0f, 1.0f, // 2: superior derecha
+	-3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,    0.0f,  0.0f, 1.0f, // 3: superior izquierda
+
+	// Back face (cara trasera, z = -3.2)
+	-3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,    0.0f,  0.0f, -1.0f, // 4: inferior izquierda
+	 3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,    0.0f,  0.0f, -1.0f, // 5: inferior derecha
+	 3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,    0.0f,  0.0f, -1.0f, // 6: superior derecha
+	-3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,    0.0f,  0.0f, -1.0f, // 7: superior izquierda
+
+	// Left face (lado izquierdo, x = -5)
+	-3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,    -1.0f, 0.0f, 0.0f, // 8: inferior atrás
+	-3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,    -1.0f, 0.0f, 0.0f, // 9: inferior adelante
+	-3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,    -1.0f, 0.0f, 0.0f, // 10: superior adelante
+	-3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,    -1.0f, 0.0f, 0.0f, // 11: superior atrás
+
+	// Right face (lado derecho, x = 5)
+	 3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,     1.0f, 0.0f, 0.0f,  // 12: inferior adelante
+	 3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,     1.0f, 0.0f, 0.0f,  // 13: inferior atrás
+	 3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,     1.0f, 0.0f, 0.0f,  // 14: superior atrás
+	 3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,     1.0f, 0.0f, 0.0f,  // 15: superior adelante
+
+	 // Top face (cara superior, y = 2)
+	 -3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,     0.0f, 1.0f, 0.0f,  // 16: frontal izquierda
+	  3.0f,  1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,     0.0f, 1.0f, 0.0f,  // 17: frontal derecha
+	  3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,     0.0f, 1.0f, 0.0f,  // 18: trasera derecha
+	 -3.0f,  1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,     0.0f, 1.0f, 0.0f,  // 19: trasera izquierda
+
+	 // Bottom face (cara inferior, y = -2)
+	 -3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  0.0f, 0.0f,     0.0f, -1.0f, 0.0f, // 20: trasera izquierda
+	  3.0f, -1.0f, -3.2f,       1.0f,1.0f,1.0f,  1.0f, 0.0f,     0.0f, -1.0f, 0.0f, // 21: trasera derecha
+	  3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  1.0f, 1.0f,     0.0f, -1.0f, 0.0f, // 22: frontal derecha
+	 -3.0f, -1.0f, -3.0f,       1.0f,1.0f,1.0f,  0.0f, 1.0f,     0.0f, -1.0f, 0.0f  // 23: frontal izquierda
+};
+
+
+GLuint wallIndices[] = {
+	// Front face
+	0, 1, 2,
+	0, 2, 3,
+	// Back face
+	4, 5, 6,
+	4, 6, 7,
+	// Left face
+	8, 9, 10,
+	8, 10, 11,
+	// Right face
+	12, 13, 14,
+	12, 14, 15,
+	// Top face
+	16, 17, 18,
+	16, 18, 19,
+	// Bottom face
+	20, 21, 22,
+	20, 22, 23
+};
+
+
 GLfloat lightVertices[] =
 {
 	-0.1f, -0.1f,  0.1f,
@@ -181,15 +258,52 @@ int main()
 	lightVBO.Unbind();
 	lightEBO.Unbind();
 
+		// VAO, VBO y EBO para el plano
+	VAO planeVAO;
+	planeVAO.Bind();
+	VBO planeVBO(planeVertices, sizeof(planeVertices));
+	EBO planeEBO(planeIndices, sizeof(planeIndices));
 
-	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	// Configuramos los atributos: posición (location 0), color (location 1),
+	// coordenadas de textura (location 2) y normales (location 3).
+	planeVAO.LinkAttrib(planeVBO, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+	planeVAO.LinkAttrib(planeVBO, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	planeVAO.LinkAttrib(planeVBO, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	planeVAO.LinkAttrib(planeVBO, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
+	planeVAO.Unbind();
+	planeVBO.Unbind();
+	planeEBO.Unbind();
+
+	VAO wallVAO;
+	wallVAO.Bind();
+	VBO wallVBO(wallVertices, sizeof(wallVertices));
+	EBO wallEBO(wallIndices, sizeof(wallIndices));
+
+	// Configuramos los atributos (asumiendo que usas 11 floats por vértice como en los otros objetos)
+	wallVAO.LinkAttrib(wallVBO, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);               // Posición
+	wallVAO.LinkAttrib(wallVBO, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float))); // Color
+	wallVAO.LinkAttrib(wallVBO, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float))); // TexCoords
+	wallVAO.LinkAttrib(wallVBO, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float))); // Normales
+
+	wallVAO.Unbind();
+	wallVBO.Unbind();
+	wallEBO.Unbind();
+
+
+	glm::vec4 lightColor = glm::vec4(0.5f, 0.5f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 1.0f, 0.5f);
 	glm::mat4 lightModel = glm::mat4(1.0f);
 	lightModel = glm::translate(lightModel, lightPos);
 
 	glm::vec3 pyramidPos = glm::vec3(0.0f, 0.0f, 0.0f);
 	glm::mat4 pyramidModel = glm::mat4(1.0f);
 	pyramidModel = glm::translate(pyramidModel, pyramidPos);
+
+	glm::mat4 floorModel = glm::mat4(1.0f);
+	floorModel = glm::translate(floorModel, glm::vec3(0.0f, -1.0f, 0.0f));
+
+	glm::mat4 wallModel = glm::mat4(1.0f);
+	wallModel = glm::translate(wallModel, glm::vec3(0.0f, -0.5f, 0.0f));
 
 
 	lightShader.Activate();
@@ -200,8 +314,14 @@ int main()
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
-	Texture brickTex("Textures/floor.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture brickTex("Textures/wood.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	brickTex.textureUnit(shaderProgram, "tex0", 0);
+
+	Texture floorTex("Textures/floor.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	floorTex.textureUnit(shaderProgram, "tex0", 0);
+
+	Texture wallTex("Textures/brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	wallTex.textureUnit(shaderProgram, "tex0", 0);
 
 	glEnable(GL_DEPTH_TEST);
 
@@ -209,7 +329,7 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
+		glClearColor(0.0f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Camara
@@ -224,10 +344,23 @@ int main()
 		VAO1.Bind();
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
+
+		floorTex.Bind();
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(floorModel));
+		planeVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(planeIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
+		
+		wallTex.Bind();
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(wallModel));
+		wallVAO.Bind();
+		glDrawElements(GL_TRIANGLES, sizeof(wallIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
 		lightShader.Activate();
 		camera.Matrix(lightShader, "camMatrix");
 		lightVAO.Bind();
 		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -236,6 +369,15 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+
+	planeVAO.Delete();
+	planeVBO.Delete();
+	planeEBO.Delete();
+	wallVAO.Delete();
+	wallVBO.Delete();
+	wallEBO.Delete();
+	wallTex.Delete();
+
 	brickTex.Delete();
 	shaderProgram.Delete();
 	lightVAO.Delete();
